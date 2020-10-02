6e5e1adaee64e5715d26dd0e1d5ee03e33a1ae05
relaxeaza <relaxeaza@outlook.com>
2020-10-01 21:57:31 -0300

    Only redirect http to https on production environment.

--- app.js
+++ app.js
@@ -21,13 +21,15 @@ const mapsRouter = require('./routes/maps')
 
 const app = express()
 
-app.use(function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] === 'https') {
-        next()
-    } else {
-        res.redirect('https://' + req.hostname + req.url)
-    }
-})
+if (process.env.NODE_DEV === 'production') {
+    app.use(function (req, res, next) {
+        if (req.headers['x-forwarded-proto'] === 'https') {
+            next()
+        } else {
+            res.redirect('https://' + req.hostname + req.url)
+        }
+    })
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
