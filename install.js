41a47ca8fd8a173c67dc4155aaa49211e01cac1a
relaxeaza <relaxeaza@outlook.com>
2020-10-08 22:11:22 -0300

    Add simple script to install systemd unit.

---
+++ install.js
@@ -0,0 +1,181 @@
+(async function () {
+    let isUser = false
+    let runAs = 'arch'
+    let environmentType
+
+    const { exec } = require('child_process')
+    const fs = require('fs').promises
+    const path = require('path')
+    const xdgBasedir = require('xdg-basedir')
+    const readline = require('readline')
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+
+    async function install () {
+        try {
+            await checkUserType()
+            await checkSystemd()
+            await checkUnitExists()
+            await setEnvironment()
+
+            if (!isUser) {
+                await selectUser()
+            }
+        
+            const unitDest = isUser ? xdgBasedir.config + '/systemd/user/tw2tracker.service' : '/etc/systemd/system/tw2tracker.service'
+
+            let unitTemplate = await fs.readFile('./share/tw2tracker.service', 'utf-8')
+
+            unitTemplate = unitTemplate.replace('${tw2tracker.environment}', environmentType)
+            unitTemplate = unitTemplate.replace('${tw2tracker.directory}', __dirname)
+            unitTemplate = unitTemplate.replace('${tw2tracker.user}', isUser ? '' : 'User=' + runAs)
+
+            await fs.writeFile(unitDest, unitTemplate)
+
+            await reloadDaemon()
+            await enableDaemon()
+
+            process.stdout.write('Unit installed in ' + unitDest + '\n')
+            process.exit(0)
+        } catch (error) {
+            if (error) {
+                process.stdout.write(error.message || error)
+            }
+
+            process.stdout.write('Exiting...\n')
+            process.exit(1)
+        }
+    }
+
+    function checkUserType () {
+        return new Promise(function (resolve, reject) {
+            if (process.env.SUDO_UID) {
+                resolve()
+            } else {
+                rl.question('You need root access to run this.\nDo you want to install with the current user? [y/n] ', function (response) {
+                    if (!response || /y|/i.test(response)) {
+                        isUser = true
+                        resolve()
+                    } else {
+                        reject()
+                    }
+                })
+            }
+        })
+    }
+
+    function checkUserType () {
+        return new Promise(function (resolve, reject) {
+            if (process.env.SUDO_UID) {
+                resolve()
+            } else {
+                rl.question('You need root access to run this.\nDo you want to install with the current user? [y/n] ', function (response) {
+                    if (!/^y|$/i.test(response)) {
+                        isUser = true
+                        resolve()
+                    } else {
+                        reject()
+                    }
+                })
+            }
+        })
+    }
+
+    function selectUser () {
+        return new Promise(function (resolve, reject) {
+            rl.question('Which user would you like the unit to run as? [Default: arch] ', async function (response) {
+                if (response === 'root') {
+                    process.stdout.write('Not allowed to run as root.')
+                    await selectUser()
+                } else if (!response) {
+                    runAs = 'arch'
+                } else {
+                    runAs = response
+                }
+
+                resolve()
+            })
+        })
+    }
+
+    function checkSystemd () {
+        return new Promise(function (resolve, reject) {
+            exec('systemctl', function (error, stdout, stderr) {
+                if (error) {
+                    reject('Systemd is not installed.')
+                } else {
+                    resolve()
+                }
+            })
+        })
+    }
+
+    function checkUnitExists () {
+        return new Promise(async function (resolve, reject) {
+            try {
+                if (isUser) {
+                    await fs.access(xdgBasedir.config + '/systemd/user/tw2tracker.service')
+                } else {
+                    await fs.access('/etc/systemd/system/tw2tracker.service')
+                }
+
+                rl.question('Systemd unit already exists, replace it? [y/n] ', function (response) {
+                    if (/^y|$/i.test(response)) {
+                        resolve()
+                    } else {
+                        reject()
+                    }
+                })
+            } catch {
+                resolve()
+            }
+        })
+    }
+
+    function reloadDaemon () {
+        return new Promise(function (resolve, reject) {
+            const command = isUser ? 'systemctl --user daemon-reload' : 'systemctl daemon-reload'
+
+            exec(command, function (error) {
+                if (error) {
+                    reject(error)
+                } else {
+                    resolve()
+                }
+            })
+        })
+    }
+
+    function enableDaemon () {
+        return new Promise(function (resolve, reject) {
+            const command = isUser ? 'systemctl --user enable tw2tracker.service' : 'systemctl enable tw2tracker.service'
+
+            rl.question('Enable unit to init on system start-up automatically? [y/n] ', function (response) {
+                if (/^y|$/i.test(response)) {
+                    exec(command, function (error) {
+                        if (error) {
+                            reject(error)
+                        } else {
+                            resolve()
+                        }
+                    })
+                } else {
+                    resolve()
+                }
+            })
+        })
+    }
+
+    function setEnvironment () {
+        return new Promise(function (resolve, reject) {
+            rl.question('Is this a development environment? [y/n] ', async function (response) {
+                environmentType = /n/i.test(response) ? 'production' : 'development'
+                resolve()
+            })
+        })
+    }
+
+    install()
+})()
