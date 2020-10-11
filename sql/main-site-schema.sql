56a342f0bd44aa38e7e56068a4eff5f0c2e67ccf
relaxeaza <relaxeaza@outlook.com>
2020-10-11 14:55:53 -0300

    Database: Update default tw account password.

--- sql/main-site-schema.sql
+++ sql/main-site-schema.sql
@@ -10,9 +10,8 @@ CREATE TABLE main.settings (
 CREATE TABLE main.markets (
     id VARCHAR (10) PRIMARY KEY,
     account_name VARCHAR (255) DEFAULT 'tribalwarstracker',
-    account_password VARCHAR (255) DEFAULT 'tribalwarstracker'
+    account_password VARCHAR (255) DEFAULT '7FONlraMpdnvrNIVE8aOgSGISVW00A'
 );
-
 CREATE TABLE main.worlds (
     market VARCHAR (10) REFERENCES main.markets(id),
     num SMALLINT,
