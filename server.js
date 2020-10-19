ef347dee08a9d743b6cb8dcd7a8589ec21f243e9
relaxeaza <relaxeaza@outlook.com>
2020-10-19 19:17:29 -0300

    Site: Style error page.

--- server.js
+++ server.js
@@ -108,7 +108,9 @@ app.use(function (err, req, res, next) {
 
     // render the error page
     res.status(err.status || 500)
-    res.render('error')
+    res.render('error', {
+        title: 'Tw2-Tracker Error'
+    })
 })
 
 app.set('port', port)
