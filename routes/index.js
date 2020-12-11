ce25662342ffbbbfef2bb98564cdc1206e919fec
relaxeaza <relaxeaza@outlook.com>
2020-12-11 11:01:08 -0300

    Stats: Add home showing all markets.

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,34 @@
 const express = require('express')
 const router = express.Router()
 const getSettings = require('../settings')
+const {db,pgp} = require('../db')
+const sql = require('../sql')
+const utils = require('../utils')
+const {asyncRouter, hasOwn} = utils
 
-router.get('/', async function (req, res, next) {
+router.get('/', asyncRouter(async function (req, res, next) {
     const settings = await getSettings()
-
-    res.render('index', {
-        title: `Home - ${settings.site_name}`
+    const worlds = await db.any(sql.worlds.all)
+    const marketsIds = Array.from(new Set(worlds.map(world => world.market)))
+    const markets = marketsIds.map(function (marketId) {
+        return {
+            id: marketId,
+            player_count: worlds.reduce((base, next) => next.market === marketId ? base + next.player_count : base, 0),
+            tribe_count: worlds.reduce((base, next) => next.market === marketId ? base + next.tribe_count : base, 0),
+            village_count: worlds.reduce((base, next) => next.market === marketId ? base + next.village_count : base, 0),
+            open_world_count: worlds.reduce((base, next) => next.market === marketId && next.open ? base + 1 : base, 0),
+            closed_world_count: worlds.reduce((base, next) => next.market === marketId && !next.open ? base + 1 : base, 0)
+        }
     })
-})
+
+    res.render('stats-home', {
+        title: settings.site_name,
+        markets,
+        navigation: [
+            `<a href="/stats/">${settings.site_name}</a>`
+        ],
+        ...utils.ejsHelpers
+    })
+}))
 
 module.exports = router
--- routes/stats.js
+++ routes/stats.js
@@ -22,6 +22,31 @@ const conquestTypes = {
     SELF: 'self'
 }
 
+router.get('/', asyncRouter(async function (req, res, next) {
+    const settings = await getSettings()
+    const worlds = await db.any(sql.worlds.all)
+    const marketsIds = Array.from(new Set(worlds.map(world => world.market)))
+    const markets = marketsIds.map(function (marketId) {
+        return {
+            id: marketId,
+            player_count: worlds.reduce((base, next) => next.market === marketId ? base + next.player_count : base, 0),
+            tribe_count: worlds.reduce((base, next) => next.market === marketId ? base + next.tribe_count : base, 0),
+            village_count: worlds.reduce((base, next) => next.market === marketId ? base + next.village_count : base, 0),
+            open_world_count: worlds.reduce((base, next) => next.market === marketId && next.open ? base + 1 : base, 0),
+            closed_world_count: worlds.reduce((base, next) => next.market === marketId && !next.open ? base + 1 : base, 0)
+        }
+    })
+
+    res.render('stats-home', {
+        title: settings.site_name,
+        markets,
+        navigation: [
+            `<a href="/stats/">${settings.site_name}</a>`
+        ],
+        ...utils.ejsHelpers
+    })
+}))
+
 router.get('/:marketId', asyncRouter(async function (req, res, next) {
     if (req.params.marketId.length !== 2) {
         return next()
