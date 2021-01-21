df3a667e3af4f9c1aba4ee4a857916db055d5d0c
relaxeaza <relaxeaza@outlook.com>
2021-01-21 16:44:31 -0300

    Simplify sql.js

--- sql.js
+++ sql.js
@@ -1,22 +1,7 @@
-// File sql.js
-
-// Proper way to organize an sql provider:
-//
-// - have all sql files for Users in ./sql/users
-// - have all sql files for Products in ./sql/products
-// - have your sql provider module as ./sql/index.js
-
 const QueryFile = require('pg-promise').QueryFile
 const path = require('path')
 const fs = require('fs')
 const rcamelcase = /-([a-z])/g
-
-const readSqlFile = function (file) {
-    const fullPath = path.join(__dirname, file)
-    return new QueryFile(fullPath, {
-        minify: true
-    })
-}
 
 const getSqlTree = function (from, sub, tree = {}) {
     sub = sub || from
@@ -29,8 +14,7 @@ const getSqlTree = function (from, sub, tree = {}) {
         if (itemStat.isFile()) {
             const fileName = path.parse(item).name
             const camelCaseName = fileName.replace(rcamelcase, (file) => file[1].toUpperCase())
-            const sqlPath = path.join(sub, item)
-            const sql = readSqlFile(sqlPath)
+            const sql = new QueryFile(path.join(__dirname, sub, item))
 
             tree[camelCaseName] = sql
         } else if (itemStat.isDirectory()) {
