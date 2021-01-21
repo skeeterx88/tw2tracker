a2844cabc2b113ce176869b0eec429f490018d08
relaxeaza <relaxeaza@outlook.com>
2021-01-21 17:37:00 -0300

    Create a default config file, generate config file if not exists.

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
 const fs = require('fs')
 const ini = require('ini')
 
+if (!fs.existsSync('./config.ini')) {
+    const defaults = fs.readFileSync('./config.default.ini', 'utf-8')
+    fs.writeFileSync('./config.ini', defaults)
+}
+
 module.exports = ini.decode(fs.readFileSync('./config.ini', 'utf-8'))
