d29a4047962c4427a0dc98b75f607c393aba4399
relaxeaza <relaxeaza@outlook.com>
2021-01-21 19:59:57 -0300

    Move default config files to share/ and generate db.ini if not exists.

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const ini = require('ini')
 
 if (!fs.existsSync('./config.ini')) {
-    const defaults = fs.readFileSync('./config.default.ini', 'utf-8')
+    const defaults = fs.readFileSync('./share/config.default.ini', 'utf-8')
     fs.writeFileSync('./config.ini', defaults)
 }
 
--- db.js
+++ db.js
@@ -1,5 +1,11 @@
 const fs = require('fs')
 const ini = require('ini')
+
+if (!fs.existsSync('./db.ini')) {
+    const defaults = fs.readFileSync('./share/db.default.ini', 'utf-8')
+    fs.writeFileSync('./db.ini', defaults)
+}
+
 const pgp = require('pg-promise')()
 const dbconfig = ini.decode(fs.readFileSync('./db.ini', 'utf-8'))
 
