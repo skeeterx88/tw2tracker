57f829c5b80dc6e165057db3638be3ad5ac19c1c
relaxeaza <relaxeaza@outlook.com>
2021-01-24 17:11:50 -0300

    Use a TOML package that supports encode.

--- config.js
+++ config.js
@@ -1,12 +1,13 @@
+const TOML = require('@iarna/toml')
 const fs = require('fs')
-const defaults = require('./share/config.defaults.toml')
+const defaults = TOML.parse(fs.readFileSync('./share/config.defaults.toml', 'utf-8'))
 const defaultsRaw = fs.readFileSync('./share/config.defaults.toml', 'utf-8')
 
 if (!fs.existsSync('./config.toml')) {
     fs.writeFileSync('./config.toml', defaultsRaw)
 }
 
-const userDefined = require('./config.toml')
+const userDefined = TOML.parse(fs.readFileSync('./config.toml', 'utf-8'))
 
 module.exports = {
     ...defaults,
