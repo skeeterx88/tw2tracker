a345e6cf600a30050ca695f5de6cba7179ef878b
relaxeaza <relaxeaza@outlook.com>
2021-03-03 20:36:44 -0300

    i18n: Create i18n function to retrieve lang strings.

--- src/i18n.js
+++ src/i18n.js
@@ -1,23 +1,28 @@
 const config = require('./config.js');
+const utils = require('./utils.js');
 const TOML = require('@iarna/toml');
 const path = require('path');
 const fs = require('fs');
-const I18N_PATH = path.join('i18n');
-const file = path.join(I18N_PATH, `${config.lang}.toml`);
+const I18N_PATH = 'i18n';
 
-if (!fs.existsSync(file)) {
-    throw new Error('The selected language does not have a language file');
+const i18n = {};
+
+for (const file of fs.readdirSync(I18N_PATH)) {
+    const id = path.parse(file).name;
+    const content = fs.readFileSync(path.join(I18N_PATH, file), 'utf-8');
+    i18n[id] = TOML.parse(content);
 }
 
-const content = fs.readFileSync(file, 'utf-8');
-module.exports = TOML.parse(content);
-
-// const i18n = {};
-
-// for (const file of fs.readdirSync(I18N_PATH)) {
-//     const id = path.parse(file).name;
-//     const content = fs.readFileSync(path.join(I18N_PATH, file), 'utf-8');
-//     i18n[id] = TOML.parse(content);
-// }
-
-// module.exports = i18n;
+module.exports = function (key, namespace, language = config.lang, tokens) {
+    try {
+        if (!i18n[language][namespace][key]) {
+            return '[' + key + ', ' + namespace + ']';
+        } else if (tokens) {
+            return utils.sprintf(i18n[language][namespace][key], tokens);
+        } else {
+            return i18n[language][namespace][key];
+        }
+    } catch (error) {
+        return '[' + key + ', ' + namespace + ']';
+    }
+}
