2765e4f9e78e7414b2bcf723cbdb09ce985735a8
relaxeaza <relaxeaza@outlook.com>
2021-03-08 10:42:50 -0300

    Fix defaults/config no merge deep values.

--- src/config.js
+++ src/config.js
@@ -1,9 +1,38 @@
 const fs = require('fs');
 const defaults = require('../share/default-config.json');
 
+function isObject (item) {
+    return (item && typeof item === 'object' && !Array.isArray(item));
+}
+
+function mergeDeep (target, ...sources) {
+    if (!sources.length) {
+        return target;
+    }
+
+    const source = sources.shift();
+
+    if (isObject(target) && isObject(source)) {
+        for (const key in source) {
+            if (isObject(source[key])) {
+                if (!target[key]) Object.assign(target, {
+                    [key]: {}
+                });
+                mergeDeep(target[key], source[key]);
+            } else {
+                Object.assign(target, {
+                    [key]: source[key]
+                });
+            }
+        }
+    }
+
+    return mergeDeep(target, ...sources);
+}
+
 if (fs.existsSync('../config.json')) {
     const config = require('../config.json');
-    module.exports = {...defaults, ...config};
+    module.exports = mergeDeep(defaults, config);
 } else {
     fs.promises.writeFile('../config.json', JSON.stringify(defaults));
     module.exports = defaults;
