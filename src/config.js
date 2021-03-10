5d342949dd25fbf51475b3ae9d7c49ee8edfb1d0
relaxeaza <relaxeaza@outlook.com>
2021-03-10 20:01:53 -0300

    Fix default config generation.

--- src/config.js
+++ src/config.js
@@ -30,10 +30,10 @@ function mergeDeep (target, ...sources) {
     return mergeDeep(target, ...sources);
 }
 
-if (fs.existsSync('../config.json')) {
+if (fs.existsSync('./config.json')) {
     const config = require('../config.json');
     module.exports = mergeDeep(defaults, config);
 } else {
-    fs.promises.writeFile('../config.json', JSON.stringify(defaults));
+    fs.promises.writeFile('./config.json', JSON.stringify(defaults));
     module.exports = defaults;
 }
