6ffbb446ec0d6a4455b5bec1a37025eff8f58236
relaxeaza <relaxeaza@outlook.com>
2021-04-05 10:03:07 -0300

    Sync: Remove debugging code.

--- src/sync.js
+++ src/sync.js
@@ -99,15 +99,6 @@ Sync.init = async function () {
 
     await Sync.initQueue('data');
     await Sync.initQueue('achievements');
-
-    // await Sync.all('data');
-    // await Sync.all('achievements');
-
-    // const worldId = 'br55';
-    // const data = JSON.parse(fs.readFileSync(path.join('.', 'data', 'raw', `${worldId}.json`), 'utf8'));
-    // await commitRawDataFilesystem(data, worldId);
-    // await commitDataDatabase(data, worldId);
-    // await commitDataFilesystem(worldId);
 };
 
 Sync.initQueue = async function (type) {
