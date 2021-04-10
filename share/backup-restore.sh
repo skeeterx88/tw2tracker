36e60d58914b929ecd5761be6db4a5e6a66b7635
relaxeaza <relaxeaza@outlook.com>
2021-04-10 08:04:40 -0300

    Extra: Fix data backup/restore script.

--- share/backup-restore.sh
+++ share/backup-restore.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-gzip --decompress /any/tw2tracker.db.gz --stdout > /any/tw2tracker.db
 dropdb --username postgres tw2tracker
 createdb --username postgres tw2tracker
-psql --username postgres --dbname tw2tracker < /any/tw2tracker.db
+gzip --decompress /any/tw2tracker.db.gz --stdout | psql --username postgres --dbname tw2tracker
 rsync --recursive --verbose /any/static-maps /var/www/tw2tracker/data/
