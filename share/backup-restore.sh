af799e3a79e4f251337133ba74ac61a467a94686
relaxeaza <relaxeaza@outlook.com>
2021-04-15 22:02:41 -0300

    Extra: Adapt backup/restore script to fixed /var/www/tw2tracker directory.

--- share/backup-restore.sh
+++ share/backup-restore.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-dropdb --username postgres tw2tracker
-createdb --username postgres tw2tracker
-gzip --decompress /any/tw2tracker.db.gz --stdout | psql --username postgres --dbname tw2tracker
-rsync --recursive --verbose /any/static-maps /var/www/tw2tracker/data/
+if dropdb --username postgres tw2tracker; then
+    createdb --username postgres tw2tracker
+    gzip --decompress /any/tw2tracker.db.gz --stdout | psql --username postgres --dbname tw2tracker
+    rsync --recursive --verbose /any/static-maps /var/www/tw2tracker/data/
+fi
