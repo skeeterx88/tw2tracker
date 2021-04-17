5ff762b56ff3b8d539330fdbaf6293fbe7ed93e9
relaxeaza <relaxeaza@outlook.com>
2021-04-17 10:43:50 -0300

    Config: Remove refresh function.

--- src/config.js
+++ src/config.js
@@ -1,31 +1,24 @@
 const fs = require('fs');
 const defaults = require('../share/default-config.json');
 const utils = require('../src/utils.js');
-const hasOwn = Object.prototype.hasOwnProperty;
-let configObj;
+let config;
 
-function config (namespace, key) {
-    if (!hasOwn.call(configObj, namespace)) {
+if (fs.existsSync('./config.json')) {
+    config = require('../config.json');
+    config = utils.mergeDeep(defaults, config);
+} else {
+    fs.promises.writeFile('./config.json', JSON.stringify(defaults));
+    config = defaults;
+}
+
+module.exports = function (namespace, key) {
+    if (!utils.hasOwn(config, namespace)) {
         throw new Error(`Config namespace "${namespace}" not found.`);
     } else if (!key) {
-        return configObj[namespace];
-    } else if (!hasOwn.call(configObj[namespace], key)) {
+        return config[namespace];
+    } else if (!utils.hasOwn(config[namespace], key)) {
         throw new Error(`Config key "${key}" from namespace "${namespace}" not found.`);
     }
 
-    return configObj[namespace][key];
-}
-
-function refresh () {
-    if (fs.existsSync('./config.json')) {
-        configObj = require('../config.json');
-        configObj = utils.mergeDeep(defaults, configObj);
-    } else {
-        fs.promises.writeFile('./config.json', JSON.stringify(defaults));
-        configObj = defaults;
-    }
-}
-
-refresh();
-config.refresh = refresh;
-module.exports = config;
+    return config[namespace][key];
+};
--- src/i18n.js
+++ src/i18n.js
@@ -1,12 +1,9 @@
 const config = require('./config.js');
+const utils = require('./utils.js');
 const languages = require('./languages.js');
+const defaultLang = config('general', 'lang');
 
-function sprintf (string, tokens = []) {
-    let i = 0;
-    return string.replace(/%{[^}]*}/g, () => tokens[i++]);
-}
-
-module.exports = function (key, namespace, language = config('general', 'lang'), tokens) {
+module.exports = function (key, namespace, language = defaultLang, tokens) {
     try {
         if (!languages[language][namespace][key]) {
             return '[' + key + ', ' + namespace + ']';
@@ -14,9 +11,9 @@ module.exports = function (key, namespace, language = config('general', 'lang'),
             const value = languages[language][namespace][key];
 
             if (Array.isArray(value)) {
-                return sprintf(Math.round(tokens[0]) === 1 ? value[0] : value[1], tokens);
+                return utils.sprintf(Math.round(tokens[0]) === 1 ? value[0] : value[1], tokens);
             } else {
-                return sprintf(value, tokens);
+                return utils.sprintf(value, tokens);
             }
         } else {
             return languages[language][namespace][key];
