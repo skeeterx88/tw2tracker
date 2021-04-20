db1bcf746f374cff8675a6e4fcb3f88de42e70c1
relaxeaza <relaxeaza@outlook.com>
2021-04-20 11:43:41 -0300

    Extra: Add script to populate the global table of players.

---
+++ share/populate-global-players-table.js
@@ -0,0 +1,36 @@
+const {db, sql} = require('../src/db.js');
+
+db.task(async function (tx) {
+    const worlds = await tx.any(sql('get-worlds'));
+    const exists = {};
+
+    for (const world of worlds) {
+        const marketId = world.market;
+        const worldNumber = world.num;
+        const worldId = world.world_id;
+
+        exists[worldId] = {};
+
+        const players = await tx.any('SELECT id, name FROM ${worldId:name}.players', {worldId});
+
+        for (const player of players) {
+            if (!exists[worldId][player.id]) {
+                await tx.query('INSERT INTO public.players (id, name, market_id) VALUES (${id}, ${name}, ${marketId}) ON CONFLICT (id, market_id) DO NOTHING', {
+                    ...player,
+                    marketId
+                });
+                exists[worldId][player.id] = 1;
+            }
+
+            await tx.query('UPDATE public.players SET worlds = ARRAY_APPEND(worlds, ${worldNumber}) WHERE id = ${id} AND market_id = ${marketId}', {
+                ...player,
+                worldNumber,
+                marketId
+            });
+        }
+
+        console.log(worldId, 'added', players.length, 'players');
+    }
+}).then(function () {
+    console.log('finished');
+});
