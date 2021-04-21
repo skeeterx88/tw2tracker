2ce1d9e3da722468802bb7aea05e7ee3f70668d3
relaxeaza <relaxeaza@outlook.com>
2021-04-20 21:45:46 -0300

    Router: Remove unused param.

--- src/routes/languages.js
+++ src/routes/languages.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const config = require('../config.js');
 const i18n = require('../i18n.js');
 
-router.get('/:lang?', function (req, res, next) {
+router.get('/:lang?', function (req, res) {
     if (req.params.lang) {
         req.session.lang = req.params.lang || config('general', 'lang');
         return res.redirect('back');
