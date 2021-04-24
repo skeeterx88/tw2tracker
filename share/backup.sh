81cbe8b056fcf6e0c77c7b7d58f5dff4fe853d54
relaxeaza <relaxeaza@outlook.com>
2021-04-24 15:59:32 -0300

    Extra: Make backup rsync display progress when downloading db dump.

--- share/backup.sh
+++ share/backup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 ssh arch@tw2tracker 'pg_dump --dbname=tw2tracker --username=postgres --verbose --compress 9 > /any/tw2tracker.db.gz'
-rsync arch@tw2tracker:/any/tw2tracker.db.gz /any/tw2tracker.db.gz
+rsync --verbose --progress arch@tw2tracker:/any/tw2tracker.db.gz /any/tw2tracker.db.gz
 rsync --recursive --verbose arch@tw2tracker:/var/www/tw2tracker/data/static-maps /any/
